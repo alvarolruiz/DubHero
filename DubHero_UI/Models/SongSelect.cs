6f79af415ac07622d49d38bd6fd12e84c292e316
georgesdsk <sadachok99@gmail.com>
2022-02-23 13:47:49 +0100

    abberrr

---
+++ DubHero_UI/Models/SongSelect.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DubHero_UI.Models
+{
+    public class SongSelect
+    {
+        public SongSelect()
+        {
+            SongList = new List<String>();
+            SongList.Add("pp");
+            SongList.Add("pp");
+            SongList.Add("pp");
+            SongList.Add("pp");
+            SongList.Add("pp");
+            SongList.Add("pp");
+
+        }
+
+        public List<String> SongList { get; set; }
+
+
+    }
+}
