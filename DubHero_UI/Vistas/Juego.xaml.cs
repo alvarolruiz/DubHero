2cff059eb099fc6313dec70548770193425d4383
Carlos Aragon Pelayo <carlos.aragon@iesnervion.es>
2022-02-23 12:20:32 +0100

    Movido y renombrado clase playback

--- DubHero_UI/Vistas/Juego.xaml.cs
+++ DubHero_UI/Vistas/Juego.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Melanchall.DryWetMidi.Common;
+﻿using DubHero_UI.Classes;
+using Melanchall.DryWetMidi.Common;
 using Melanchall.DryWetMidi.Core;
 using Melanchall.DryWetMidi.Multimedia;
 using System;
@@ -30,13 +31,16 @@ namespace DubHero_UI.Vistas
     /// </summary>
     public sealed partial class Juego : Page
     {
+        private PlaybackManager _playback;
 
         public Juego()
         {
             this.InitializeComponent();
 
-            myPlayBack myPlayBack = new myPlayBack();
+             _playback = new PlaybackManager();
         }
+
+        
 
         //    while (myPlayBack.ReproductorMidi.IsRunning)
         //    {
@@ -124,81 +128,6 @@ namespace DubHero_UI.Vistas
             pistaObjetivo.Children.Add(rec);
             return rec;
         }
-
-
-    }
-
-
-
-    public class myPlayBack
-    {
-        private Playback reproductorMidi;
-        private MidiFile midiFile;
-        private SevenBitNumber note;
-
-
-
-
-        public myPlayBack()
-        {
-            midiFile = MidiFile.Read("Assets/sevenNationArmyMap.mid");
-            this.reproductorMidi = midiFile.GetPlayback();
-            this.reproductorMidi.NoteCallback += EventoNota;
-
-
-            //(rawNoteData, rawTime, rawLength, playbackTime) =>
-
-            // new NotePlaybackData(
-            // rawNoteData.NoteNumber,
-            // rawNoteData.Velocity,     // leave velocity as is
-            // rawNoteData.OffVelocity,  // leave off velocity as is
-            // rawNoteData.Channel);
-            // reproductorMidi.EventPlayed += ReproductorMidi_EventPlayed;
-            // reproductorMidi.TrackNotes = true;
-            
-
-        }
-
-        public void empezarMidi() {
-
-            reproductorMidi.Start();
-
-        }
-
-
-        private NotePlaybackData EventoNota(NotePlaybackData rawData, long rawTime, long rawLength, TimeSpan playbackTime)
-        {
-            
-
-            return rawData;
-
-        }
-
-        private delegate crearAnimacion();
-
-
-        private void ReproductorMidi_EventPlayed(object sender, MidiEventPlayedEventArgs e)
-        {
-
-            if (e.Event.EventType == MidiEventType.NoteOn)
-            {
-
-
-            }
-            var evento = (NoteEvent)e.Event;
-            int numero = evento.NoteNumber;
-
-        }
-
-        public Playback ReproductorMidi { get => reproductorMidi; set => reproductorMidi = value; }
-        public SevenBitNumber Note { get => note; set => note = value; }
-
-
-
-
-
-
-
     }
 }
 
