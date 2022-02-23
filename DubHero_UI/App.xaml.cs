1a2362a40c4f621716427ddf6afdd166ccdd9fc8
LuisMZ <luismezu@gmail.com>
2022-02-23 13:32:03 +0100

    altavoz con animacion pulse done

--- DubHero_UI/App.xaml.cs
+++ DubHero_UI/App.xaml.cs
@@ -67,7 +67,7 @@ namespace DubHero_UI
                     // Cuando no se restaura la pila de navegación, navegar a la primera página,
                     // configurando la nueva página pasándole la información requerida como
                     //parámetro de navegación
-                    rootFrame.Navigate(typeof(Juego), e.Arguments);
+                    rootFrame.Navigate(typeof(BlankPage1), e.Arguments);
                 }
                 // Asegurarse de que la ventana actual está activa.
                 Window.Current.Activate();
