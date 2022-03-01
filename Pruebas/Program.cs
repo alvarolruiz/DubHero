0316c5fc3299d9e565431a6d1a07ac3b53ad588b
Alvaro Lauriño <alvarolaurino@gmail.com>
2022-03-01 20:50:55 +0100

    altavozJuego

---
+++ Pruebas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Pruebas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string docPath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName);
+            Console.WriteLine(docPath);
+            string cancion;
+
+            List<string> dirs = new List<string>(Directory.EnumerateDirectories(@"..\..\..\Assets\songs"));
+
+            foreach (var dir in dirs)
+            {
+                cancion = $"{dir.Substring(dir.LastIndexOf(Path.DirectorySeparatorChar) + 1)}";
+                Console.WriteLine($"{dir.Substring(dir.LastIndexOf(Path.DirectorySeparatorChar) + 1)}");
+            }
+        }
+    }
+}
