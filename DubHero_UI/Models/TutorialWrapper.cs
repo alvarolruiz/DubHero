baf890cb736743bdd090ff821ea8eb7d1b2d94d0
Alvaro Lauriño <alvarolaurino@gmail.com>
2022-03-07 09:43:29 +0100

    La vista de juego recibe un songView Completo

--- DubHero_UI/Models/TutorialWrapper.cs
+++ DubHero_UI/Models/TutorialWrapper.cs
@@ -10,16 +10,16 @@ namespace DubHero_UI.Models
     public class TutorialWrapper
     {
 
-        private String songName;
+        private SongView songView;
         private MediaTimelineController _mediaTimelineController;
 
-        public TutorialWrapper(string songName, MediaTimelineController mediaTimelineController)
+        public TutorialWrapper(SongView songView, MediaTimelineController mediaTimelineController)
         {
-            this.songName = songName;
+            this.SongView = songView;
             this._mediaTimelineController = mediaTimelineController;
         }
 
-        public string SongName { get => songName; set => songName = value; }
         public MediaTimelineController MediaTimelineController { get => _mediaTimelineController; set => _mediaTimelineController = value; }
+        public SongView SongView { get => songView; set => songView=value; }
     }
 }
